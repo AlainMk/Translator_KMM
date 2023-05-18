9900436270938618f7f4b58386abee848e7de6ca
Alain MK <42724442+AlainMk@users.noreply.github.com>
2023-05-18 14:13:47 +0100

    setting up the translation client

---
+++ shared/src/commonMain/kotlin/com/alainmk/translator_kmm/translate/domain/translate/TranslateClient.kt
@@ -0,0 +1,11 @@
+package com.alainmk.translator_kmm.translate.domain.translate
+
+import com.alainmk.translator_kmm.core.domain.language.Language
+
+interface TranslateClient {
+    suspend fun translate(
+        fromLanguage: Language,
+        fromText: String,
+        toLanguage: Language
+    ): String
+}
