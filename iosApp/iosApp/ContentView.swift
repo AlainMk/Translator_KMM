5757a54cbfc7aca0c670bb941d118a71dd187f3c
Alain MK <42724442+AlainMk@users.noreply.github.com>
2023-05-19 17:40:58 +0100

    building translation history and more

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -2,18 +2,22 @@ import SwiftUI
 import shared
 
 struct ContentView: View {
-	private let appModule = AppModule()
-
-	var body: some View {
-        TranslateScreen(
-            historyDataSource: appModule.historyDataSource,
-            translateUseCase: appModule.translateUseCase
-        )
-	}
+    private let appModule = AppModule()
+    
+    var body: some View {
+        ZStack {
+            Color.background
+                .ignoresSafeArea()
+            TranslateScreen(
+                historyDataSource: appModule.historyDataSource,
+                translateUseCase: appModule.translateUseCase
+            )
+        }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
-	static var previews: some View {
-		ContentView()
-	}
+    static var previews: some View {
+        ContentView()
+    }
 }
