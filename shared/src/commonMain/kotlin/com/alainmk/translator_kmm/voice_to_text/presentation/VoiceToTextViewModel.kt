c9261f8a000d73ffbb7df7b21e042eabbaa94023
Alain MK <42724442+AlainMk@users.noreply.github.com>
2023-05-21 02:13:16 +0100

    implementing voice to text ui with all the logics

--- shared/src/commonMain/kotlin/com/alainmk/translator_kmm/voice_to_text/presentation/VoiceToTextViewModel.kt
+++ shared/src/commonMain/kotlin/com/alainmk/translator_kmm/voice_to_text/presentation/VoiceToTextViewModel.kt
@@ -18,12 +18,17 @@ class VoiceToTextViewModel(
     val state = _state.combine(parser.state) { state, voiceResult ->
         state.copy(
             spokenText = voiceResult.result,
-            recordError = voiceResult.error,
+            recordError = if (state.canRecord) {
+                voiceResult.error
+            } else {
+                "Can't record without permission"
+            },
             displayState = when {
-                voiceResult.error != null -> DisplayState.ERROR
+                !state.canRecord || voiceResult.error != null -> DisplayState.ERROR
                 voiceResult.result.isNotBlank() && !voiceResult.isSpeaking -> {
                     DisplayState.DISPLAYING_RESULTS
                 }
+
                 voiceResult.isSpeaking -> DisplayState.SPEAKING
                 else -> DisplayState.WAITING_TO_TALK
             }
@@ -34,11 +39,13 @@ class VoiceToTextViewModel(
 
     init {
         viewModelScope.launch {
-            while(true) {
-                if(state.value.displayState == DisplayState.SPEAKING) {
-                    _state.update { it.copy(
-                        powerRatios = it.powerRatios + parser.state.value.powerRatio
-                    ) }
+            while (true) {
+                if (state.value.displayState == DisplayState.SPEAKING) {
+                    _state.update {
+                        it.copy(
+                            powerRatios = it.powerRatios + parser.state.value.powerRatio
+                        )
+                    }
                 }
                 delay(50L)
             }
@@ -46,22 +53,25 @@ class VoiceToTextViewModel(
     }
 
     fun onEvent(event: VoiceToTextEvent) {
-        when(event) {
+        when (event) {
             is VoiceToTextEvent.PermissionResult -> {
                 _state.update { it.copy(canRecord = event.isGranted) }
             }
+
             VoiceToTextEvent.Reset -> {
                 parser.reset()
                 _state.update { VoiceToTextState() }
             }
+
             is VoiceToTextEvent.ToggleRecording -> toggleRecording(event.languageCode)
             else -> Unit
         }
     }
 
     private fun toggleRecording(languageCode: String) {
+        _state.update { it.copy(powerRatios = emptyList()) }
         parser.cancel()
-        if(state.value.displayState == DisplayState.SPEAKING) {
+        if (state.value.displayState == DisplayState.SPEAKING) {
             parser.stopListening()
         } else {
             parser.startListening(languageCode)
