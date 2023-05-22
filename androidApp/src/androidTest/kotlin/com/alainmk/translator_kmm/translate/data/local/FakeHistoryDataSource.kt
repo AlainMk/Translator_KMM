86eba8db7f27902b132172fedb86bff0b2a14845
Alain MK <42724442+AlainMk@users.noreply.github.com>
2023-05-22 15:05:57 +0100

    implementing E2E ui test in android

---
+++ androidApp/src/androidTest/kotlin/com/alainmk/translator_kmm/translate/data/local/FakeHistoryDataSource.kt
@@ -0,0 +1,20 @@
+package com.alainmk.translator_kmm.translate.data.local
+
+import com.alainmk.translator_kmm.core.domain.util.CommonFlow
+import com.alainmk.translator_kmm.core.domain.util.toCommonFlow
+import com.alainmk.translator_kmm.translate.domain.history.HistoryDataSource
+import com.alainmk.translator_kmm.translate.domain.history.HistoryItem
+import kotlinx.coroutines.flow.MutableStateFlow
+
+class FakeHistoryDataSource: HistoryDataSource {
+
+    private val _data = MutableStateFlow<List<HistoryItem>>(emptyList())
+
+    override fun getHistory(): CommonFlow<List<HistoryItem>> {
+        return _data.toCommonFlow()
+    }
+
+    override suspend fun insertHistoryItem(item: HistoryItem) {
+        _data.value += item
+    }
+}
